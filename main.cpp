625c3563014150626a2a7979acb31e07a8421d4d
keshavbhatt <keshavnrj@gmail.com>
2020-03-16 23:41:14 +0530

    app now accept --set argument this will be used in autostart function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "darkstyle.h"
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QApplication::setApplicationName("BingWall");
     QApplication::setOrganizationName("org.keshavnrj.ubuntu");
-
     a.setStyle(new DarkStyle);
     MainWindow w;
-    w.show();
+
+
+    QStringList argsList = a.arguments();
+    if(argsList.contains("--set",Qt::CaseSensitive)){
+        //set wallpaper of the day in hidden mode, usually used when app is set to run on startup
+        //procedure
+        //hide mainwindow, load wallpapers, if loaded, select first and set it as wallpaper,quit the app
+        w.setWallpaperOfTheDay();
+    }else{
+        w.show();
+    }
 
     return a.exec();
 }
