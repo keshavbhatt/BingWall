a5abc8bc5ada3767409f5b4b7b79060cca382784
keshavbhatt <keshavnrj@gmail.com>
2020-04-14 20:08:20 +0530

    project update

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     QStringList argsList = a.arguments();
     if(argsList.contains("--set",Qt::CaseSensitive)){
         //set wallpaper of the day in hidden mode, usually used when app is set to run on startup
-        //procedure
+        //procedure:
         //hide mainwindow, load wallpapers, if loaded, select first and set it as wallpaper,quit the app
         w.setWallpaperOfTheDay();
     }else{
